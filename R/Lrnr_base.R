dc37cd222593b11c3e16bf68280ef816eeecfdec
Jonathan Levy <jl@jlstiles.com>
2018-05-14 11:57:18 -0700

    fixing interaction nonsense

--- R/Lrnr_base.R
+++ R/Lrnr_base.R
@@ -36,17 +36,17 @@ Lrnr_base <- R6Class(
       if (is.null(params)) {
         params <- list(...)
       }
-
+      
       private$.params <- params
       private$.learner_uuid <- UUIDgenerate(use.time = TRUE)
-
+      
       invisible(self)
     },
-
+    
     subset_covariates = function(task) {
       # allows learners to use only a subset of covariates
       if ("covariates" %in% names(self$params) &&
-        !is.null(self$params[["covariates"]])) {
+          !is.null(self$params[["covariates"]])) {
         task_covariates <- task$nodes$covariates
         subset_covariates <- intersect(task_covariates, self$params$covariates)
         return(task$next_in_chain(covariates = subset_covariates))
@@ -54,7 +54,7 @@ Lrnr_base <- R6Class(
         return(task)
       }
     },
-
+    
     get_outcome_type = function(task) {
       outcome_type <- task$outcome_type
       if (!is.null(self$params$outcome_type)) {
@@ -67,14 +67,14 @@ Lrnr_base <- R6Class(
       }
       return(outcome_type)
     },
-
+    
     base_train = function(task, trained_sublearners = NULL) {
       # trains learner to data
       assert_that(is(task, "sl3_Task"))
-
+      
       # TODO: add error handling
       subsetted_task <- self$subset_covariates(task)
-
+      
       if (!is.null(trained_sublearners)) {
         fit_object <- private$.train(subsetted_task, trained_sublearners)
       } else {
@@ -84,7 +84,7 @@ Lrnr_base <- R6Class(
       new_object$set_train(fit_object, task)
       return(new_object)
     },
-
+    
     set_train = function(fit_object, training_task) {
       private$.fit_object <- fit_object
       save_training <- getOption("sl3.save.training")
@@ -94,7 +94,7 @@ Lrnr_base <- R6Class(
       private$.training_outcome_type <- self$get_outcome_type(training_task)
       private$.fit_uuid <- UUIDgenerate(use.time = TRUE)
     },
-
+    
     assert_trained = function() {
       if (!self$is_trained) {
         stop(paste(
@@ -103,34 +103,30 @@ Lrnr_base <- R6Class(
         ))
       }
     },
-
+    
     base_predict = function(task = NULL) {
       self$assert_trained()
       if (is.null(task)) {
         task <- private$.training_task
-      } else {
-        task <- task$next_in_chain(covariates <-
-          private$.training_task$nodes$covariates)
       }
+      
       assert_that(is(task, "sl3_Task"))
       subsetted_task <- self$subset_covariates(task)
       predictions <- private$.predict(subsetted_task)
-
+      
       ncols <- ncol(predictions)
       if (!is.null(ncols) && (ncols == 1)) {
         predictions <- as.vector(predictions)
       }
       return(predictions)
     },
-
+    
     base_chain = function(task = NULL) {
       self$assert_trained()
       if (is.null(task)) {
         task <- private$.training_task
-      } else {
-        task <- task$next_in_chain(covariates <-
-          private$.training_task$nodes$covariates)
       }
+      
       assert_that(is(task, "sl3_Task"))
       subsetted_task <- self$subset_covariates(task)
       # use custom chain function if provided
@@ -141,38 +137,38 @@ Lrnr_base <- R6Class(
       }
       return(next_task)
     },
-
+    
     train_sublearners = function(task) {
       return(private$.train_sublearners(task))
     },
-
+    
     train = function(task) {
       delayed_fit <- delayed_learner_train(self, task)
       return(delayed_fit$compute())
     },
-
+    
     predict = function(task = NULL) {
       delayed_preds <- delayed_learner_fit_predict(self, task)
       return(delayed_preds$compute())
     },
-
+    
     chain = function(task = NULL) {
       delayed_chained <- delayed_learner_fit_chain(self, task)
       return(delayed_chained$compute())
     },
-
+    
     print = function() {
       print(self$name)
       # print(self$params)
       fit_object <- private$.fit_object
       if (!is.null(fit_object)) print(fit_object)
     },
-
+    
     custom_chain = function(new_chain_fun = NULL) {
       private$.custom_chain <- new_chain_fun
     }
   ),
-
+  
   active = list(
     is_trained = function() {
       return(!is.null(private$.fit_object))
@@ -181,7 +177,7 @@ Lrnr_base <- R6Class(
       fit_object <- private$.fit_object
       return(fit_object)
     },
-
+    
     name = function() {
       # TODO: allow custom names
       if (is.null(private$.name)) {
@@ -197,31 +193,31 @@ Lrnr_base <- R6Class(
       }
       return(private$.name)
     },
-
+    
     learner_uuid = function() {
       return(private$.learner_uuid)
     },
-
+    
     fit_uuid = function() {
       return(private$.fit_uuid)
     },
-
+    
     params = function() {
       return(private$.params)
     },
-
+    
     training_task = function() {
       return(private$.training_task)
     },
-
+    
     training_outcome_type = function() {
       return(private$.training_outcome_type)
     },
-
+    
     properties = function() {
       return(private$.properties)
     },
-
+    
     coefficients = function() {
       self$assert_trained()
       coefs <- try(coef(self$fit_object))
@@ -232,7 +228,7 @@ Lrnr_base <- R6Class(
       }
     }
   ),
-
+  
   private = list(
     .name = NULL,
     .fit_object = NULL,
@@ -244,24 +240,24 @@ Lrnr_base <- R6Class(
     .required_packages = NULL,
     .properties = list(),
     .custom_chain = NULL,
-
+    
     .train_sublearners = function(task) {
       # train sublearners here
       return(NULL)
     },
-
+    
     .train = function(task) {
       stop(paste(
         "Learner is meant to be abstract, you should instead use",
         "specific learners. See listLearners()"
       ))
     },
-
+    
     .predict = function(task) {
       predictions <- predict(private$.fit_object, newdata = task$X)
       return(predictions)
     },
-
+    
     .chain = function(task) {
       predictions <- self$predict(task)
       predictions <- as.data.table(predictions)
@@ -273,7 +269,7 @@ Lrnr_base <- R6Class(
         column_names = new_col_names
       ))
     },
-
+    
     .load_packages = function() {
       if (!is.null(private$.required_packages)) {
         requirePackages(
--- R/sl3_Task.R
+++ R/sl3_Task.R
@@ -28,24 +28,22 @@ sl3_Task <- R6Class(
   class = TRUE,
   public = list(
     initialize = function(data, covariates, outcome = NULL, outcome_type = NULL,
-                              outcome_levels = NULL, id = NULL, weights = NULL,
-                              offset = NULL, nodes = NULL, column_names = NULL,
-                              row_index = NULL, folds = NULL) {
-
+                          outcome_levels = NULL, id = NULL, weights = NULL,
+                          offset = NULL, nodes = NULL, column_names = NULL,
+                          row_index = NULL, folds = NULL) {
+      
       # process data
       assert_that(is.data.frame(data) | is.data.table(data))
       private$.data <- data
-
-      if (!inherits(data, "data.table")) {
-        setDT(private$.data)
-      }
-
+      
+      setDT(private$.data)
+      
       # process column_names
       if (is.null(column_names)) {
         column_names <- as.list(names(data))
         names(column_names) <- column_names
       }
-
+      
       private$.column_names <- column_names
       # generate node list from other arguments
       if (is.null(nodes)) {
@@ -56,11 +54,11 @@ sl3_Task <- R6Class(
       } else {
         # TODO: validate node schema
       }
-
+      
       # verify nodes are contained in dataset
       all_nodes <- unlist(nodes)
       missing_cols <- setdiff(all_nodes, names(column_names))
-
+      
       assert_that(
         length(missing_cols) == 0,
         msg = sprintf(
@@ -68,9 +66,9 @@ sl3_Task <- R6Class(
           paste(missing_cols, collapse = " ")
         )
       )
-
+      
       private$.nodes <- nodes
-
+      
       # process outcome type
       if (is.character(outcome_type)) {
         outcome_type <- variable_type(
@@ -84,27 +82,25 @@ sl3_Task <- R6Class(
           outcome_type <- variable_type("none")
         }
       }
-
+      
       private$.outcome_type <- outcome_type
-
+      
       # process row_index
       private$.row_index <- row_index
       private$.folds <- folds
-
+      
       # assign uuid
       private$.uuid <- UUIDgenerate(use.time = T)
       invisible(self)
     },
-
-    add_interactions = function(interactions) {
+    
+    add_interactions = function(interactions, warn_on_existing = TRUE) {
       ## ------------------------------------------------------------------------
       ## Add columns with interactions (by reference) to input design matrix
       ## (data.table). Used for training / predicting.
       ## returns the names of the added columns
       ## ------------------------------------------------------------------------
-      data.table::setDF(private$.data)
-      data.table::setDT(private$.data)
-
+      
       prod.DT <- function(x) {
         y <- x[[1]]
         for (i in 2:ncol(x)) {
@@ -112,103 +108,104 @@ sl3_Task <- R6Class(
         }
         return(y)
       }
-
+      
       old_names <- names(private$.data)
       interaction_names <- names(interactions)
       for (i in seq_along(interactions)) {
         interact <- interactions[[i]]
         name <- interaction_names[i]
-
+        
         if (is.null(name) || is.na(name)) {
           name <- interaction_names[i] <- paste0(interact, collapse = "_")
         }
-
+        
         if (name %in% old_names) {
-          # this column is already defined, so warn but don't recalculate
-          warning(sprintf(
-            "Interaction column %s is already defined, so skipping",
-            name
-          ))
+          if(warn_on_existing){
+            # this column is already defined, so warn but don't recalculate
+            warning(sprintf(
+              "Interaction column %s is already defined, so skipping",
+              name
+            ))
+          }
         } else if (all(interact %in% old_names)) {
           private$.data[, (name) := prod.DT(.SD), .SD = interact]
         }
       }
-
-
+      
+      
       interaction_names <- intersect(interaction_names, names(private$.data))
       # drop interactions that didn't get made
       private$.column_names[interaction_names] <- interaction_names
       new_covariates <- c(self$nodes$covariates, interaction_names)
       return(self$next_in_chain(covariates = new_covariates))
     },
-
+    
     add_columns = function(fit_uuid, new_data, global_cols = FALSE) {
-      data <- private$.data
-      current_cols <- names(data)
-
+      current_cols <- names(private$.data)
+      
       if (!(is.data.frame(new_data) | is.data.table(new_data))) {
         new_data <- as.data.table(new_data)
       }
-
+      
       setDT(new_data)
       col_names <- names(new_data)
       original_names <- copy(col_names)
-
+      
       if (!global_cols) {
         # by default prepend column names with fit_uuid to prevent column name
         # conflicts for multiple fits from same learner
         col_names <- paste(fit_uuid, original_names, sep = "_")
         # setnames(new_data, original_names, col_names)
       }
-
+      
       column_names <- private$.column_names
       column_names[original_names] <- col_names
-
+      
       if (is.null(private$.row_index)) {
-        set(data, j = col_names, value = new_data)
+        set(private$.data, j = col_names, value = new_data)
       } else {
-        set(data, i = private$.row_index, j = col_names, value = new_data)
+        set(private$.data, i = private$.row_index, j = col_names, value = new_data)
       }
-
+      
       # return an updated column_names map
       return(column_names)
     },
-
+    
     next_in_chain = function(covariates = NULL, outcome = NULL, id = NULL,
-                                 weights = NULL, offset = NULL, column_names = NULL,
-                                 new_nodes = NULL, ...) {
+                             weights = NULL, offset = NULL, column_names = NULL,
+                             new_nodes = NULL, ...) {
       if (is.null(new_nodes)) {
         new_nodes <- self$nodes
-
+        
         if (!is.null(covariates)) {
           new_nodes$covariates <- covariates
         }
-
+        
         if (!is.null(outcome)) {
           new_nodes$outcome <- outcome
         }
-
+        
         if (!missing(id)) {
           new_nodes$id <- id
         }
-
+        
         if (!missing(weights)) {
           new_nodes$weights <- weights
         }
-
+        
         if (!missing(offset)) {
           new_nodes$offset <- offset
         }
       }
-
+      
       if (is.null(column_names)) {
         column_names <- private$.column_names
       }
       all_nodes <- unlist(new_nodes)
-
+      
       # verify nodes are contained in dataset
       missing_cols <- setdiff(all_nodes, names(column_names))
-
+      
       assert_that(
         length(missing_cols) == 0,
         msg = sprintf(
@@ -216,12 +213,12 @@ sl3_Task <- R6Class(
           paste(missing_cols, collapse = " ")
         )
       )
-
+      
       new_task <- self$clone()
-
+      
       if ((is.null(new_nodes$outcome) &&
-        is.null(self$nodes$outcome)) ||
-        (new_nodes$outcome == self$nodes$outcome)) {
+           is.null(self$nodes$outcome)) ||
+          (new_nodes$outcome == self$nodes$outcome)) {
         # if we have the same outcome, transfer outcome properties
         new_outcome_type <- self$outcome_type
       } else {
@@ -237,7 +234,7 @@ sl3_Task <- R6Class(
       )
       return(new_task)
     },
-
+    
     subset_task = function(row_index) {
       old_row_index <- private$.row_index
       if (!is.null(old_row_index)) {
@@ -255,30 +252,30 @@ sl3_Task <- R6Class(
       )
       return(new_task)
     },
-
+    
     get_data = function(rows = NULL, columns, expand_factors = FALSE) {
       if (missing(rows)) {
         rows <- private$.row_index
       }
-
+      
       true_columns <- unlist(private$.column_names[columns])
-
+      
       if (!is.null(rows)) {
         subset <- private$.data[rows, true_columns, with = FALSE]
       } else {
         subset <- private$.data[, true_columns, with = FALSE]
       }
-
+      
       if (ncol(subset) > 0) {
         data.table::setnames(subset, true_columns, columns)
       }
-
+      
       if (expand_factors) {
         subset <- dt_expand_factors(subset)
       }
       return(subset)
     },
-
+    
     has_node = function(node_name) {
       node_var <- private$.nodes[[node_name]]
       return(!is.null(node_var))
@@ -290,12 +287,12 @@ sl3_Task <- R6Class(
         }
       }
       node_var <- private$.nodes[[node_name]]
-
+      
       if (is.null(node_var)) {
         return(generator_fun(node_name, self$nrow))
       } else {
         data_col <- self$get_data(, node_var, expand_factors)
-
+        
         if (ncol(data_col) == 1) {
           return(unlist(data_col, use.names = FALSE))
         } else {
@@ -306,7 +303,7 @@ sl3_Task <- R6Class(
     offset_transformed = function(link_fun = NULL, for_prediction = FALSE) {
       if (self$has_node("offset")) {
         offset <- self$offset
-
+        
         # transform if sl3.transform.offset is true and link_fun was provided
         if (getOption("sl3.transform.offset") && !is.null(link_fun)) {
           offset <- link_fun(offset)
@@ -317,23 +314,23 @@ sl3_Task <- R6Class(
       }
       return(offset)
     },
-
+    
     print = function() {
       cat(sprintf("A sl3 Task with %d obs and these nodes:\n", self$nrow))
       print(self$nodes)
     }
   ),
-
+  
   active = list(
     raw_data = function() {
       return(private$.data)
     },
-
+    
     data = function() {
       all_nodes <- unlist(private$.nodes)
       return(self$get_data(, all_nodes))
     },
-
+    
     nrow = function() {
       if (is.null(private$.row_index)) {
         return(nrow(private$.data))
@@ -341,55 +338,55 @@ sl3_Task <- R6Class(
         return(length(private$.row_index))
       }
     },
-
+    
     nodes = function() {
       return(private$.nodes)
     },
-
+    
     X = function() {
       covariates <- private$.nodes$covariates
       X_dt <- self$get_data(, covariates, expand_factors = TRUE)
       return(X_dt)
     },
-
+    
     X_intercept = function() {
       # returns X matrix with manually generated intercept column
       X_dt <- self$X
-
+      
       if (ncol(X_dt) == 0) {
         intercept <- rep(1, self$nrow)
         X_dt <- self$data[, list(intercept = intercept)]
       } else {
         old_ncol <- ncol(X_dt)
         X_dt[, intercept := 1]
-
+        
         # make intercept first column
         setcolorder(X_dt, c(old_ncol + 1, seq_len(old_ncol)))
       }
-
+      
       return(X_dt)
     },
-
+    
     Y = function() {
       return(self$get_node("outcome"))
     },
-
+    
     offset = function() {
       return(self$get_node("offset"))
     },
-
+    
     weights = function() {
       return(self$get_node("weights", function(node_var, n) {
         rep(1, n)
       }))
     },
-
+    
     id = function() {
       return(self$get_node("id", function(node_var, n) {
         seq_len(n)
       }))
     },
-
+    
     folds = function(new_folds) {
       if (!missing(new_folds)) {
         private$.folds <- new_folds
@@ -404,20 +401,20 @@ sl3_Task <- R6Class(
       }
       return(private$.folds)
     },
-
+    
     uuid = function() {
       return(private$.uuid)
     },
-
+    
     column_names = function() {
       return(private$.column_names)
     },
-
+    
     outcome_type = function() {
       return(private$.outcome_type)
     }
   ),
-
+  
   private = list(
     .data = NULL,
     .nodes = NULL,
